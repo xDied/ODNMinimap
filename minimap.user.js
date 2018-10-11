c82ea9c751057fe05bed1b7c054b16dd471a80e6
xDied <40411795+xDied@users.noreply.github.com>
2018-10-11 17:03:24 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/ConsoleBey/TurkeyZoneMinimap/master/';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/xDied/ODNMinimap/master/';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
