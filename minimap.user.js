4f063b30f76e0c6ea4683cd83c9a812b7bcea94f
xDied <40411795+xDied@users.noreply.github.com>
2018-10-13 21:37:58 -0300

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -62,7 +62,6 @@ window.addEventListener('load', function () {
         '<canvas id="minimap-board" style="width: 100%; height: 100%;z-index:2;position:absolute;top:0;left:0;"></canvas>' +
         '<canvas id="minimap-cursor" style="width: 100%; height: 100%;z-index:3;position:absolute;top:0;left:0;"></canvas>' +
         '</div><div id="minimap-config" style="line-height:20px;">' +
-      		     '<a href=https://discord.me/odn target="_blank">Discord' +
         '<span id="hide-map" style="cursor:pointer;color:white"> Esconder' +
         '</span> | <span id="follow-mouse" style="cursor:pointer;"Seguir mouse' +
         '</span> | Zoom: <span id="zoom-plus" style="cursor:pointer;font-weight:bold;">+</span>  /  ' +
